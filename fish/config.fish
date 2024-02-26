13ce34bca0c081a5b849fdcd214f626768bf9be0
pective <139483386+pective@users.noreply.github.com>
2024-02-26 12:37:34 +0100

    Add new Hyprland and fish configs

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     cat ~/.cache/wal/sequences
-    shutthefetchup
+    afetch
     alias vim="nvim"
     alias ls="eza -al --color=always --group-directories-first"
 end
